adbc889be10a51be6bf7b30da93f6d5edf34b97a
Dickson <dicksonmelo@gmail.com>
2024-02-25 17:23:15 -0300

    change variable names

--- conversor.go
+++ conversor.go
@@ -8,41 +8,41 @@ import (
 )
 
 func main() {
-	binary_number := os.Args[1]
+	binaryInput := os.Args[1]
 
-	binary_numbers := strings.Split(binary_number, ",")
-	after_comma := binary_numbers[0]
+	binaryParts := strings.Split(binaryInput, ",")
+	afterComma := binaryParts[0]
 
-	var decimal_based_after_comma []int
-	var decimal_based_before_comma []int
-	var decimal_integer int
-	var decimal_float_part int
+	var decimalAfterCommaList []int
+	var decimalBeforeCommaList []int
+	var decimalIntegerPart int
+	var decimalFloatPart int
 
-	for index := 0; index < len(after_comma); index++ {
-		integer := int(after_comma[index] - '0')
-		potency := float64(len(after_comma) - 1 - index)
+	for index := 0; index < len(afterComma); index++ {
+		integer := int(afterComma[index] - '0')
+		potency := float64(len(afterComma) - 1 - index)
 		decimal_based_integer := integer * int(math.Pow(2, potency))
-		decimal_based_after_comma = append(decimal_based_after_comma, decimal_based_integer)
+		decimalAfterCommaList = append(decimalAfterCommaList, decimal_based_integer)
 	}
 
-	for index := 0; index < len(decimal_based_after_comma); index++ {
-		decimal_integer += decimal_based_after_comma[index]
+	for index := 0; index < len(decimalAfterCommaList); index++ {
+		decimalIntegerPart += decimalAfterCommaList[index]
 	}
 
-	if len(binary_numbers) > 1 {
-		before_comma := binary_numbers[1]
+	if len(binaryParts) > 1 {
+		beforeComma := binaryParts[1]
 
-		for index := 0; index < len(before_comma); index++ {
-			integer := int(before_comma[index] - '0')
-			potency := float64(len(before_comma) - 1 - index)
-			decimal_before_comma := integer * int(math.Pow(2, potency))
-			decimal_based_before_comma = append(decimal_based_before_comma, decimal_before_comma)
+		for index := 0; index < len(beforeComma); index++ {
+			integer := int(beforeComma[index] - '0')
+			potency := float64(len(beforeComma) - 1 - index)
+			decimalBeforeComma := integer * int(math.Pow(2, potency))
+			decimalBeforeCommaList = append(decimalBeforeCommaList, decimalBeforeComma)
 		}
 
-		for index := 0; index < len(decimal_based_before_comma); index++ {
-			decimal_float_part += decimal_based_before_comma[index]
+		for index := 0; index < len(decimalBeforeCommaList); index++ {
+			decimalFloatPart += decimalBeforeCommaList[index]
 		}
 	}
 
-	fmt.Println(float64(decimal_integer) + (float64(decimal_float_part) / 10))
+	fmt.Println(float64(decimalIntegerPart) + (float64(decimalFloatPart) / 10))
 }
